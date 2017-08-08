2728b71b85ff33ba6aa73c2bf7ddd32bdacdb26e
Lowton <iskeyiv@gmail.com>
2017-08-08 10:08:15 +0400

    Add files
    Add all existing files to the project

---
+++ RMB.py
@@ -0,0 +1,2 @@
+import telepot
+import config, log
---
+++ config.py
@@ -0,0 +1,6 @@
+#Telegram
+token = '284465130:AAGil5Wf-16wHooCJkrLhPtvMHDk9mEqPB0'
+
+#Logging
+loggerName = 'RaidManagerBotLog'
+logFile = 'rmb.log'
