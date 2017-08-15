b049dd7a4d9808aeb7b146e1ee2941f2635bf3b1
Lowton <iskeyiv@gmail.com>
2017-08-15 17:02:05 +0400

    Новые функции
    Внёс новые функции
    Заглушку на команды и сообщения + начал обработку координат

--- RMB.py
+++ RMB.py
@@ -30,7 +30,33 @@ def off(bot, update):
 off_handler = CommandHandler('off', off)
 dispatcher.add_handler(off_handler)
 
+def location(bot, update):
+    user = update.message.from_user
+    log.info(user.username + ' прислал координаты: ' + update.message.location.longitude + ';' + update.message.location.latitude)
+    update.message.reply_text("Координаты: " + update.message.location.longitude + ';' + update.message.location.latitude)
 
+location_handler = LocationHandler(Filters.location, location)
+dispatcher.add_handler(location_handler)
+
+
+# записывает всё что прислали
+def other(bot,update):
+    user = update.message.from_user
+    log.info(user.username + ' написал: ' + update.message.text)
+    update.message.reply_text("Не знаю как реагировать.")
+
+other_handler = MessageHandler(Filters.text, other)
+dispatcher.add_handler(other_handler)
+
+# ответ на неизвестную команду
+def unknown(bot,update):
+    user = update.message.from_user
+    log.info(user.username + ' прислал команду: ' + update.message.text)
+    update.message.reply_text("Нет такой команды!")
+
+# Задаём хендлер для всех остальных команд (заглушку)
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
 
 #Запускаем бота
 updater.start_polling()
