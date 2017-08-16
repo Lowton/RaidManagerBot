5169d08eef1b1628d38d3b6131dc4b47ffe87120
Lowton <iskeyiv@gmail.com>
2017-08-16 14:52:21 +0400

    Причесал
    Изменил стуктуру в соответствии с рекомендацией по телеботам от производителя

--- RMB.py
+++ RMB.py
@@ -3,21 +3,13 @@ from telegram.ext import (Updater, CommandHandler,
 from log import log
 import config
 
-
-log.info('Бот запущен.')
-
-updater = Updater(token=config.token)
-dispatcher = updater.dispatcher
-
+# При старте
 def start(bot, update):
     start_text = 'Бот предназначен для организиции рейдов на боссов игроков PokemonGO.\n'
     user = update.message.from_user
     log.info('Вызвано приветствие ({})'.format(user.username))
     bot.send_message(chat_id=update.message.chat_id,
                      text=start_text)
-
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
 
 # Функция отключения бота
 def off(bot, update):
@@ -27,21 +19,15 @@ def off(bot, update):
                      text="Выключаюсь…")
     updater.stop()
 
-off_handler = CommandHandler('off', off)
-dispatcher.add_handler(off_handler)
-
+# определение координат
 def location(bot, update):
     user = update.message.from_user
     user_location = update.message.location
-    log.info('{} прислал координаты: {}, {}'.format(user.username,
-                                                   user_location.latitude,
-                                                   user_location.longitude))
-    update.message.reply_text("Координаты: {}, {}".format(user_location.latitude,user_location.longitude))
-
-location_handler = MessageHandler(Filters.location, location)
-dispatcher.add_handler(location_handler)
-
-
+    log.info('Координаты: {}, {} ({})'.format(user_location.latitude,
+                                              user_location.longitude,
+                                              user.username))
+    update.message.reply_text("Координаты: {}, {}".format(user_location.latitude,
+                                                          user_location.longitude))
 
 # Помощь
 def help(bot, update):
@@ -51,17 +37,11 @@ def help(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                      text=help_text)
 
-help_handler = CommandHandler('help', help)
-dispatcher.add_handler(help_handler)
-
 # записывает всё что прислали
 def other(bot,update):
     user = update.message.from_user
     log.info('{} ({})'.format(update.message.text,user.username))
     update.message.reply_text("Не знаю как реагировать.")
-
-other_handler = MessageHandler(Filters.text, other)
-dispatcher.add_handler(other_handler)
 
 # ответ на неизвестную команду
 def unknown(bot,update):
@@ -69,9 +49,23 @@ def unknown(bot,update):
     log.info('Команда: {} ({})'.format(update.message.text,user.username))
     update.message.reply_text("Нет такой команды!")
 
-# Задаём хендлер для всех остальных команд (заглушку)
-unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
 
-#Запускаем бота
-updater.start_polling()
+def main():
+    log.info('Бот запущен.')
+
+    updater = Updater(token=config.token)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('off', off))
+    dp.add_handler(CommandHandler('help', help))
+    dp.add_handler(MessageHandler(Filters.location, location))
+    dp.add_handler(MessageHandler(Filters.text, other))
+    dp.add_handler(MessageHandler(Filters.command, unknown))
+
+    # Запускаем бота
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
