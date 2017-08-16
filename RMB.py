09e38c2d18d974478ee628b3576d31d57db3906b
Lowton <iskeyiv@gmail.com>
2017-08-16 13:34:02 +0400

    location
    Настроил рабочую функцию получения координат

--- RMB.py
+++ RMB.py
@@ -12,7 +12,7 @@ dispatcher = updater.dispatcher
 def start(bot, update):
     start_text = 'Бот предназначен для организиции рейдов на боссов игроков PokemonGO.\n'
     user = update.message.from_user
-    log.info('Вызвано приветствие (' + user.username + ')')
+    log.info('Вызвано приветствие ({})'.format(user.username))
     bot.send_message(chat_id=update.message.chat_id,
                      text=start_text)
 
@@ -22,7 +22,7 @@ dispatcher.add_handler(start_handler)
 # Функция отключения бота
 def off(bot, update):
     user = update.message.from_user
-    log.info('Отключение бота (' + user.username + ')')
+    log.info('Отключение бота ({})'.format(user.username))
     bot.send_message(chat_id=update.message.chat_id,
                      text="Выключаюсь…")
     updater.stop()
@@ -32,17 +32,32 @@ dispatcher.add_handler(off_handler)
 
 def location(bot, update):
     user = update.message.from_user
-    log.info(user.username + ' прислал координаты: ' + update.message.location.longitude + ';' + update.message.location.latitude)
-    update.message.reply_text("Координаты: " + update.message.location.longitude + ';' + update.message.location.latitude)
+    user_location = update.message.location
+    log.info('{} прислал координаты: {}, {}'.format(user.username,
+                                                   user_location.latitude,
+                                                   user_location.longitude))
+    update.message.reply_text("Координаты: {}, {}".format(user_location.latitude,user_location.longitude))
 
-location_handler = LocationHandler(Filters.location, location)
+location_handler = MessageHandler(Filters.location, location)
 dispatcher.add_handler(location_handler)
 
+
+
+# Помощь
+def help(bot, update):
+    user = update.message.from_user
+    help_text = "Справка.\nЭто заглушка для справки. Существут три команды:\n/start\n/help\n/off"
+    log.info('Запрос справки ({})'.format(user.username))
+    bot.send_message(chat_id=update.message.chat_id,
+                     text=help_text)
+
+help_handler = CommandHandler('help', help)
+dispatcher.add_handler(help_handler)
 
 # записывает всё что прислали
 def other(bot,update):
     user = update.message.from_user
-    log.info(user.username + ' написал: ' + update.message.text)
+    log.info('{} ({})'.format(update.message.text,user.username))
     update.message.reply_text("Не знаю как реагировать.")
 
 other_handler = MessageHandler(Filters.text, other)
@@ -51,7 +66,7 @@ dispatcher.add_handler(other_handler)
 # ответ на неизвестную команду
 def unknown(bot,update):
     user = update.message.from_user
-    log.info(user.username + ' прислал команду: ' + update.message.text)
+    log.info('Команда: {} ({})'.format(update.message.text,user.username))
     update.message.reply_text("Нет такой команды!")
 
 # Задаём хендлер для всех остальных команд (заглушку)
